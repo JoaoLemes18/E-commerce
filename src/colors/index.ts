c56a282ae84809f48da266ffeec6e2992c20fd5e
João Lemes <joaolemes17@hotmail.com>
2023-10-29 23:26:24 -0400

    feat: add colors in the project

---
+++ src/colors/index.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primaryColor: "#1A1A1A",
+  backgroundColor: "#0B0B0B",
+  secondaryColor: "#2A2A2A",
+  buttonColor: "#5033C3",
+  textColor: "#676767",
+  mutedColor: "#A1A1A1",
+  accentColor: "#8162FF",
+  whiteColor: "#FFFFFF",
+};
