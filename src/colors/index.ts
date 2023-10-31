ea54b7a532670e4a956b153243d6963ad131ec15
João Lemes <joaolemes17@hotmail.com>
2023-10-30 22:44:20 -0400

    style: defined theme the app

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,10 +1,10 @@
 export const colors = {
-  primaryColor: "#1A1A1A",
   backgroundColor: "#0B0B0B",
+  primaryColor: "#1A1A1A",
   secondaryColor: "#2A2A2A",
-  buttonColor: "#5033C3",
   textColor: "#676767",
   mutedColor: "#A1A1A1",
-  accentColor: "#8162FF",
   whiteColor: "#FFFFFF",
+  buttonColor: "#5033C3",
+  accentColor: "#8162FF",
 };
