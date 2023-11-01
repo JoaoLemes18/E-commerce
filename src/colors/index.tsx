feb8a9697596f649c8b27a22c90695c3ba5da236
João Lemes <joaolemes17@hotmail.com>
2023-11-01 08:41:12 -0400

    fix: adding the gradient correctly

--- src/colors/index.tsx
+++ src/colors/index.tsx
@@ -1,7 +1,14 @@
 import LinearGradient from "react-native-linear-gradient";
 
-export const gradientcard = {
-  buttonGradient: (
+export const colors = {
+  backgroundColor: "#0B0B0B",
+  primaryColor: "#1A1A1A",
+  secondaryColor: "#2A2A2A",
+  textColor: "#676767",
+  mutedColor: "#A1A1A1",
+  whiteColor: "#FFFFFF",
+  buttonColor: "#5033C3",
+  CardGradient: (
     <LinearGradient
       colors={["#5033C3", "rgba(80, 51, 195, 0.20)"]}
       start={{ x: 0, y: 0 }}
@@ -10,4 +17,5 @@ export const gradientcard = {
       style={{ flex: 1 }}
     />
   ),
+  accentColor: "#8162FF",
 };
